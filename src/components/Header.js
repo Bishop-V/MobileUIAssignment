3eedbaba6509e0a8f5937d30caa505aa2e0bbd88
Phobos_Tau <phobos@roscosmos.net>
2026-09-17 11:22:59 -0600

    header nav/lang

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Image, Pressable } from "react-native";
+import { View, StyleSheet, Image, Pressable, Text } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import global from "../styles/global";
 export default function Header() {
@@ -10,42 +10,84 @@ export default function Header() {
   };
   return (
     <View style={styles.header}>
-      <Image
-        style={{
-          height: 60,
+      <View style={styles.row}>
+        <Image
+          style={{
+            height: 60,
 
-          width: 230,
+            width: 230,
 
-          resizeMode: "contain",
-          tintColor: "white",
-        }}
-        source={require("../assets/wordmark.png")}
-      />
+            resizeMode: "contain",
+            tintColor: "white",
+          }}
+          source={require("../assets/wordmark.png")}
+        />
 
-      <View
-        style={{
-          flexDirection: "row",
-          gap: 40,
-        }}
-      >
-        <Pressable onPress={handlePress} onLongPress={handleLongPress}>
-          <Ionicons name="grid-outline" style={global.icon}></Ionicons>
-        </Pressable>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 40,
+          }}
+        >
+          <Pressable onPress={handlePress}>
+            <Ionicons name="grid-outline" style={global.icon}></Ionicons>
+          </Pressable>
+          <Pressable onPress={handlePress}>
+            <Ionicons
+              name="notifications-outline"
+              style={global.icon}
+            ></Ionicons>
+          </Pressable>
+        </View>
+      </View>
+      <View style={styles.row}>
+        {/* community/for you */}
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 40,
+          }}
+        >
+          <Pressable onPress={handlePress} onLongPress={handleLongPress}>
+            <Text style={styles.currentPage}>Community</Text>
+            {/* underline */}
+            <View
+              style={{
+                borderColor: "#6699FF",
+                borderRadius: 4,
+                marginTop: 1,
+                borderWidth: 1.4,
+              }}
+            />
+          </Pressable>
+          <Pressable onPress={handlePress} onLongPress={handleLongPress}>
+            <Text style={styles.page}>For you</Text>
+          </Pressable>
+        </View>
+        {/* language */}
         <Pressable onPress={handlePress}>
-          <Ionicons name="notifications-outline" style={global.icon}></Ionicons>
+          <Ionicons name="language-outline" style={global.icon}></Ionicons>
         </Pressable>
       </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  currentPage: {
+    color: "#6699FF",
+  },
+  page: {},
   header: {
-    flexDirection: "row",
+    flexDirection: "column",
     width: "100%",
-    justifyContent: "space-between",
-    alignItems: "center",
+
     marginTop: "1%",
     paddingHorizontal: "3%",
     height: "5%",
+  },
+  row: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
